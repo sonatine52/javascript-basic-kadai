270393876b8b25e4aa0029f27f49e3920887936e
sonatine52 <135916531+sonatine52@users.noreply.github.com>
2023-06-17 15:43:24 +0900

    Add files via upload

---
+++ kadai_019/event.js
@@ -0,0 +1,9 @@
+// btnのidを持つHTML要素を取得し定数に代入する
+const btn = document.getElementById('btn');
+// textのidを持つHTML要素を取得し定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => { 
+  text.textContent = 'ボタンをクリックしました';
+})
